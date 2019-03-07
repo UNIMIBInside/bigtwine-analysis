43fd428dc98a716d454fafbb23ca1130cc8b82bb
Fausto <fausto91@gmail.com>
2019-03-07 23:04:39 +0100

    Definita ricerca cronologia analisi per id analisi

--- src/main/java/it/unimib/disco/bigtwine/services/analysis/repository/AnalysisStatusHistoryRepository.java
+++ src/main/java/it/unimib/disco/bigtwine/services/analysis/repository/AnalysisStatusHistoryRepository.java
@@ -1,9 +1,13 @@
 package it.unimib.disco.bigtwine.services.analysis.repository;
 
+import it.unimib.disco.bigtwine.services.analysis.domain.Analysis;
 import it.unimib.disco.bigtwine.services.analysis.domain.AnalysisStatusHistory;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 /**
@@ -12,5 +16,6 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface AnalysisStatusHistoryRepository extends MongoRepository<AnalysisStatusHistory, String> {
-
+    List<AnalysisStatusHistory> findByAnalysisId(String id, Sort sort);
+    List<AnalysisStatusHistory> findByAnalysisId(String id);
 }
