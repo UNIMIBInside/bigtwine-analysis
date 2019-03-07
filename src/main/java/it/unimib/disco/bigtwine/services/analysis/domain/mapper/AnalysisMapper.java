dc14b04d03e016c85718672289b292f240e8dfad
Fausto <fausto91@gmail.com>
2019-03-07 23:05:50 +0100

    Definiti mapper entity-dto

--- src/main/java/it/unimib/disco/bigtwine/services/analysis/domain/mapper/AnalysisMapper.java
+++ src/main/java/it/unimib/disco/bigtwine/services/analysis/domain/mapper/AnalysisMapper.java
@@ -1,12 +1,22 @@
 package it.unimib.disco.bigtwine.services.analysis.domain.mapper;
 
 import it.unimib.disco.bigtwine.services.analysis.domain.Analysis;
+import it.unimib.disco.bigtwine.services.analysis.domain.AnalysisStatusHistory;
+import it.unimib.disco.bigtwine.services.analysis.domain.enumeration.AnalysisErrorCode;
+import it.unimib.disco.bigtwine.services.analysis.domain.enumeration.AnalysisStatus;
+import it.unimib.disco.bigtwine.services.analysis.domain.enumeration.AnalysisVisibility;
 import it.unimib.disco.bigtwine.services.analysis.web.api.model.AnalysisDTO;
+import it.unimib.disco.bigtwine.services.analysis.web.api.model.AnalysisStatusEnum;
+import it.unimib.disco.bigtwine.services.analysis.web.api.model.AnalysisStatusHistoryDTO;
+import it.unimib.disco.bigtwine.services.analysis.web.api.model.AnalysisVisibilityEnum;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
 import java.time.Instant;
 import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.TimeZone;
 
 @Mapper
@@ -24,5 +34,25 @@ public interface AnalysisMapper {
 
     Analysis analysisFromAnalysisDTO(AnalysisDTO analysisDTO);
 
+    List<Analysis> analysesFromAnalysisDTOs(List<AnalysisDTO> analysisDTOs);
+
     AnalysisDTO analysisDtoFromAnalysis(Analysis analysis);
+
+    List<AnalysisDTO> analysisDtosFromAnalyses(List<Analysis> analyses);
+
+    AnalysisVisibility visibilityFromVisibilityEnum(AnalysisVisibilityEnum visibility);
+
+    AnalysisStatus statusFromStatusEnum(AnalysisStatusEnum status);
+
+    AnalysisStatusHistoryDTO statusHistoryDTOFromStatusHistory(AnalysisStatusHistory statusHistory);
+
+    List<AnalysisStatusHistoryDTO> statusHistoryDTOsFromStatusHistories(List<AnalysisStatusHistory> statusHistories);
+
+    default AnalysisErrorCode analysisErrorCodeFromInt(Integer value) {
+        return AnalysisErrorCode.valueOf(value);
+    }
+
+    default Integer intFromAnalysisErrorCode(AnalysisErrorCode value) {
+        return value.getValue();
+    }
 }
