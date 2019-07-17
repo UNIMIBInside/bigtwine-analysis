36264e0d83ef5fe8f3b267f9906864a16cb33fbc
Fausto <fausto91@gmail.com>
2019-07-17 22:24:11 +0200

    Semplificato enum AnalysisInputType

--- src/main/java/it/unimib/disco/bigtwine/services/analysis/domain/enumeration/AnalysisInputType.java
+++ src/main/java/it/unimib/disco/bigtwine/services/analysis/domain/enumeration/AnalysisInputType.java
@@ -1,23 +1,10 @@
 package it.unimib.disco.bigtwine.services.analysis.domain.enumeration;
 
-import it.unimib.disco.bigtwine.services.analysis.domain.AnalysisInput;
-import it.unimib.disco.bigtwine.services.analysis.domain.DatasetAnalysisInput;
-import it.unimib.disco.bigtwine.services.analysis.domain.QueryAnalysisInput;
-
 /**
  * The AnalysisInputType enumeration.
  */
 public enum AnalysisInputType {
-    QUERY(Constants.QUERY_VALUE, QueryAnalysisInput.class),
-    DATASET(Constants.DATASET_VALUE, DatasetAnalysisInput.class);
-
-    public final String value;
-    public final Class<? extends AnalysisInput> inputClass;
-
-    AnalysisInputType(String value, Class<? extends AnalysisInput> inputClass) {
-        this.value = value;
-        this.inputClass = inputClass;
-    }
+    QUERY, DATASET;
 
     public static class Constants {
         public static final String QUERY_VALUE = "QUERY";
