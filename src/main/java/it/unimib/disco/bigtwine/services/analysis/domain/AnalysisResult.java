900cd27cb7dedcb4a3744db496725ed06652e92c
Fausto <fausto91@gmail.com>
2019-09-21 20:07:53 +0200

    Definiti indici mongodb AnalysisResult

--- src/main/java/it/unimib/disco/bigtwine/services/analysis/domain/AnalysisResult.java
+++ src/main/java/it/unimib/disco/bigtwine/services/analysis/domain/AnalysisResult.java
@@ -3,6 +3,8 @@ package it.unimib.disco.bigtwine.services.analysis.domain;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import it.unimib.disco.bigtwine.services.analysis.config.Constants;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -13,6 +15,11 @@ import java.io.Serializable;
 import java.time.Instant;
 
 @Document(collection = Constants.ANALYSIS_RESULTS_DB_COLLECTION)
+@CompoundIndexes({
+    @CompoundIndex(def = "{'analysis': 1}", name = "analysis"),
+    @CompoundIndex(def = "{'payload.entities.category': 1}", name = "entity_category"),
+    @CompoundIndex(def = "{'payload.status.text': 'text'}", name = "payload.status.text_text")
+})
 public class AnalysisResult<P extends AnalysisResultPayload> implements Serializable {
     private static final long serialVersionUID = 1L;
 
